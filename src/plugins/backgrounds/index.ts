4eab1c07bf03433045ee8c6415eea6445a5867ef
BookCatKid <Simonhroff@icloud.com>
2025-02-09 14:36:05 -0800

    Add online image to backgrounds #656

--- src/plugins/backgrounds/index.ts
+++ src/plugins/backgrounds/index.ts
@@ -1,9 +1,11 @@
 import colour from "./colour";
 import gradient from "./gradient";
 import image from "./image";
+import online from "./online";
 import unsplash from "./unsplash";
+import giphy from "./giphy";
 import apod from "./apod";
 
-export const backgroundConfigs = [colour, gradient, image, unsplash, apod];
+export const backgroundConfigs = [colour, giphy, gradient, image, online, unsplash, apod];
 
 backgroundConfigs.sort((a, b) => a.name.localeCompare(b.name));
