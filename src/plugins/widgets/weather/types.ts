f2cfce304823f3cdbd98d84c15d582692a7b2803
BookCatKid <Simonhroff@icloud.com>
2025-02-09 10:56:28 -0800

    Revert "[FEATURE] Add 'show city name' setting to the Weather plugin #615"
    This reverts commit 32c8aa039e70ae3b105a6887ae69ba5d09bc6ae2, reversing
    changes made to 5fcf997674f811cd3b373da2e74fcd01232ba2f6.

--- src/plugins/widgets/weather/types.ts
+++ src/plugins/widgets/weather/types.ts
@@ -9,7 +9,6 @@ export type Coordinates = {
 export type Data = Coordinates & {
   name?: string;
   showDetails: boolean;
-  showCity: boolean;
   units: "auto" | "si" | "us"; // `auto` has been removed, but may still be present in settings
 };
 
@@ -24,6 +23,5 @@ export type Props = API<Data, Cache>;
 
 export const defaultData: Data = {
   showDetails: false,
-  showCity: true,
   units: "si",
 };
