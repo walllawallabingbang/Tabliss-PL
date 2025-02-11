065940e733f45b7473e4c635c14af33f2356beff
BookCatKid <Simonhroff@icloud.com>
2025-02-10 17:44:51 -0800

    Removed sentry integration
    This project appears to have used sentry for error logging. I removed all the sentry code and instead replaced it with mock code that just console.errors. Somebody could get rid of the entire file, I don't really care.

--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -1,29 +1,4 @@
-import { init, captureException, setTag } from "@sentry/browser";
-
-export function register(): void {
-  init({
-    autoSessionTracking: false, // Wtf sentry
-    dsn: "https://2e0e75c7477c4c3e9572ee97241e569c@o113629.ingest.sentry.io/250221",
-    enabled: !DEV,
-    release: VERSION,
-  });
-  setTag("target", BUILD_TARGET);
-}
-
+// Removed sentry integration (I don't have sentry)
 export function capture(error: Error): void {
-  if (error.stack) {
-    // Replace firefox extension URLs
-    error.stack = error.stack.replace(
-      /moz-extension:\/\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/g,
-      "resource://tabliss-extension",
-    );
-
-    // Replace chrome extension URLs
-    error.stack = error.stack.replace(
-      /chrome-extension:\/\/hipekcciheckooncpjeljhnekcoolahp/g,
-      "resource://tabliss-extension",
-    );
-  }
-
-  captureException(error);
+  console.error("Captured error:", error);
 }
