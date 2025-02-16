18aedf3f8191f76b7d4663373fb53f24d613d251
BookCatKid <Simonhroff@icloud.com>
2025-02-16 14:30:16 -0800

    CLOSES: Do not disable the Custom CSS widget when hiding all widgets #242
    Actually pretty useful if want to make the setting icon less visible, or reduce the opacity of the body, or whatever.

--- src/views/dashboard/Widgets.tsx
+++ src/views/dashboard/Widgets.tsx
@@ -9,21 +9,27 @@ const Widgets: React.FC = () => {
   const focus = useValue(db, "focus");
   const widgets = useSelector(db, selectWidgets);
 
-  // TODO: one day we'll have `Array.groupBy` accepted by tc39
-  const grouped = widgets.reduce<
-    Partial<Record<WidgetPosition, WidgetState[]>>
-  >(
-    (carry, widget) => ({
-      ...carry,
-      [widget.display.position]: [
-        ...(carry[widget.display.position] ?? []),
+    // NOTE: old todo: (probobly no longer relevent though) (from original maintainer) one day we'll have `Array.groupBy` accepted by tc39
+
+  const grouped: Partial<Record<WidgetPosition, WidgetState[]>> = {};
+  const cssWidgets: Partial<Record<WidgetPosition, WidgetState[]>> = {};
+
+  widgets.forEach((widget) => {
+    if (widget.key === "widget/css") {
+      cssWidgets[widget.display.position] = [
+        ...(cssWidgets[widget.display.position] ?? []),
         widget,
-      ],
-    }),
-    {},
-  );
+      ];
+    } else {
+      grouped[widget.display.position] = [
+        ...(grouped[widget.display.position] ?? []),
+        widget,
+      ];
+    }
+  });
 
   const slots = Object.entries(grouped) as [WidgetPosition, WidgetState[]][];
+  const cssSlots = Object.entries(cssWidgets) as [WidgetPosition, WidgetState[]][];
 
   return (
     <div className="Widgets fullscreen">
@@ -32,6 +38,9 @@ const Widgets: React.FC = () => {
           slots.map(([position, widgets]) => (
             <Slot key={position} position={position} widgets={widgets} />
           ))}
+        {cssSlots.map(([position, widgets]) => (
+          <Slot key={`css-${position}`} position={position} widgets={widgets} />
+        ))}
       </div>
     </div>
   );
