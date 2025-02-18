bd5c23ef5d717366e32b2571a004ab7cba77a6b3
BookCatKid <Simonhroff@icloud.com>
2025-02-17 18:40:29 -0800

    Make the copyright part of the apod background aligned to the left to use less space
    They were stacked on top of each other before, which as not consistent wit other background and wasted a bit of space.

--- src/plugins/backgrounds/apod/ApodTitle.tsx
+++ src/plugins/backgrounds/apod/ApodTitle.tsx
@@ -4,10 +4,11 @@ import { Image } from "./types";
 type Props = Pick<Image, "title" | "copyright">;
 
 const Credit: React.FC<Props> = ({ title, copyright }) => (
-  <div className="title">
+  <div className="title" style={{ lineHeight: 0 }}>
     <p>{title}</p>
-    {copyright && <p>&copy; {copyright}</p>}
+    {copyright && <p style={{ textAlign: "right" }}>&copy; {copyright}</p>}
   </div>
 );
 
 export default Credit;
+
