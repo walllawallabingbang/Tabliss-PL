4a12388a17c64cd46a33c60de01f2fd5f9841132
BookCatKid <Simonhroff@icloud.com>
2025-02-18 21:11:57 -0800

    Fix the paths that stopped faivcons from working on github pages

--- src/plugins/widgets/ipInfo/api.ts
+++ src/plugins/widgets/ipInfo/api.ts
@@ -6,7 +6,7 @@ export async function getIpInfo(loader: API["loader"]): Promise<IpData> {
 
   const primary = fetch("https://ipwho.is")
     .then((res) => res.json())
-    .catch(() => fetch("https://ipinfo.io/json?inc=ip,city,country") // FALLBACK: The request has been failing due to adblockers, but this one seems to work. other options: ip,city,region,country,loc,org,postal
+    .catch(() => fetch("https://ipinfo.io/json?inc=ip,city,country,region") // FALLBACK: The request has been failing due to adblockers, but this one seems to work. other options: ip,city,region,country,loc,org,postal
       .then((res) => res.json()));
 
   const data = await primary.finally(() => loader.pop());
