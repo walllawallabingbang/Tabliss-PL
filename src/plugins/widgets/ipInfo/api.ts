d216d09ee2deaab84f5a638fd3448d8d9f83a32e
BookCatKid <Simonhroff@icloud.com>
2025-02-18 20:51:45 -0800

    Add a fallback if the ip api is blocked by an ad-blocker

--- src/plugins/widgets/ipInfo/api.ts
+++ src/plugins/widgets/ipInfo/api.ts
@@ -4,13 +4,17 @@ import { IpData } from "./types";
 export async function getIpInfo(loader: API["loader"]): Promise<IpData> {
   loader.push();
 
-  const data = await fetch("https://ipwho.is/")
+  const primary = fetch("https://ipwho.is")
     .then((res) => res.json())
-    .finally(() => loader.pop());
+    .catch(() => fetch("https://ipinfo.io/json?inc=ip,city,country") // FALLBACK: The request has been failing due to adblockers, but this one seems to work. other options: ip,city,region,country,loc,org,postal
+      .then((res) => res.json()));
+
+  const data = await primary.finally(() => loader.pop());
 
   return {
     ip: data.ip,
-    city: data.city,
+    city: data.city || data.region || "",
     country: data.country,
   };
 }
+
