3b4a3ba9d888e7d62e8e3591eebf8e0aec39feb0
BookCatKid <Simonhroff@icloud.com>
2025-02-26 17:15:17 -0800

    Add more error handling for invalid plugin

--- src/plugins/backgrounds/media/index.ts
+++ src/plugins/backgrounds/media/index.ts
@@ -3,7 +3,7 @@ import Media from "./Media";
 import ImageSettings from "./MediaSettings";
 
 const config: Config = {
-  key: "background/media",
+  key: "background/image", // not changing to /media for backwards compatibility.
   name: "Upload Images & Videos",
   description: "See your own media.",
   dashboardComponent: Media,
--- src/plugins/plugins.ts
+++ src/plugins/plugins.ts
@@ -1,15 +1,29 @@
 import { backgroundConfigs } from "./backgrounds";
 import { widgetConfigs } from "./widgets";
+import React from "react";
 
 export { backgroundConfigs } from "./backgrounds";
 export { widgetConfigs } from "./widgets";
 
 const configs = [...backgroundConfigs, ...widgetConfigs];
 
+const DefaultComponent: React.FC = () => "Component not available";
+
 export function getConfig(key: string) {
   const config = configs.find((config) => config.key === key);
 
-  if (!config) throw new Error(`Unable to find config for plugin: ${key}`);
+  // if (!config) throw new Error(`Unable to find config for plugin: ${key}`); // WHY WOULD YOU BREAK THE ENTIRE APP IF A PLUGIN IS MISSING
+  if (!config) {
+    console.error(`Unable to find config for plugin: ${key}`);
+    return {
+      key,
+      name: key,
+      description: "The plugin id is not valid, if you need help, create an issue on the github repo.",
+      settingsComponent: DefaultComponent,
+      supportsBackdrop: false,
+      dashboardComponent: DefaultComponent,
+    };
+  }
 
   return config;
 }
